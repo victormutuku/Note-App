f42f2ad940cadc39e6753d003cc8d80147c6b610
Victor Mutuku <victor.mutuku@strathmore.edu>
2023-02-05 18:10:46 +0300

    Gridview built dynamically cased on device size

--- lib/widgets/notes_grid.dart
+++ lib/widgets/notes_grid.dart
@@ -9,18 +9,26 @@ class NotesGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<Notes>(
-      builder: (context, notes, child) => GridView.builder(
-        gridDelegate:
-            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-        itemCount: notes.fetchedNotes.length,
-        itemBuilder: (context, i) => Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: GridTile(
-            child: NoteCard(
-              id: notes.fetchedNotes[i]['id'],
-              title: notes.fetchedNotes[i]['title'],
-              text: notes.fetchedNotes[i]['text'],
+    final deviceWidth = MediaQuery.of(context).size.width;
+    final deviceHeight = MediaQuery.of(context).size.height;
+    return OrientationBuilder(
+      builder: (context, orientation) => Consumer<Notes>(
+        builder: (context, notes, child) => GridView.builder(
+          gridDelegate:
+              // const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+              SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: orientation == Orientation.portrait
+                      ? deviceWidth / 2
+                      : deviceHeight / 2),
+          itemCount: notes.fetchedNotes.length,
+          itemBuilder: (context, i) => Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GridTile(
+              child: NoteCard(
+                id: notes.fetchedNotes[i]['id'],
+                title: notes.fetchedNotes[i]['title'],
+                text: notes.fetchedNotes[i]['text'],
+              ),
             ),
           ),
         ),
