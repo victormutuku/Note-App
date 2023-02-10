5ee29ba2f9a45d4f57b51a458dcea3ad052caaf0
Victor Mutuku <victor.mutuku@strathmore.edu>
2023-02-10 11:37:16 +0300

    Able to select note widgets on pressing Edit

--- lib/widgets/notes_grid.dart
+++ lib/widgets/notes_grid.dart
@@ -3,9 +3,11 @@ import 'package:provider/provider.dart';
 
 import '../utils/notes.dart';
 import './note_card.dart';
+import './selectable_note_card.dart';
 
 class NotesGrid extends StatelessWidget {
-  const NotesGrid({super.key});
+  final bool editSelected;
+  const NotesGrid(this.editSelected,{super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -14,17 +16,19 @@ class NotesGrid extends StatelessWidget {
     return OrientationBuilder(
       builder: (context, orientation) => Consumer<Notes>(
         builder: (context, notes, child) => GridView.builder(
-          gridDelegate:
-              // const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-              SliverGridDelegateWithMaxCrossAxisExtent(
-                  maxCrossAxisExtent: orientation == Orientation.portrait
-                      ? deviceWidth / 2
-                      : deviceHeight / 2),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: orientation == Orientation.portrait
+                  ? deviceWidth / 2
+                  : deviceHeight / 2),
           itemCount: notes.fetchedNotes.length,
           itemBuilder: (context, i) => Padding(
             padding: const EdgeInsets.all(8.0),
             child: GridTile(
-              child: NoteCard(
+              child: editSelected == false ? NoteCard(
+                id: notes.fetchedNotes[i]['id'],
+                title: notes.fetchedNotes[i]['title'],
+                text: notes.fetchedNotes[i]['text'],
+              ):SelectableNoteCard(
                 id: notes.fetchedNotes[i]['id'],
                 title: notes.fetchedNotes[i]['title'],
                 text: notes.fetchedNotes[i]['text'],
